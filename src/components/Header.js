c6b6c50682bcc7b26562ddbbe10d871286991009
Jose Saul Serrano <Jose Saul Serrano>
2021-12-03 16:07:01 -0500

    the Navigation menu was styled

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,13 @@
 //libraries and hooks
 //components
 import { Navbar } from './Navbar';
-//assets
-import logo from '../assets/images/logo.svg';
 import { Logo } from './Logo';
+//assets
+import styles from "../assets/styles/Header.module.css";
 
 export const Header = () => {
    return (
-      <header className="Header">
+      <header className={styles.Header}>
          <section className="Header__logo">
             <Logo color="#000" />
          </section>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,10 +1,14 @@
 //libraries and hooks
+import { useState } from "react";
 //components
 import { Button } from "./Button";
 import { Link } from "./Link";
 //assets
+import styles from "../assets/styles/Navbar.module.css";
 
 export const Navbar = () => {
+   const [isOpen, setIsOpen] = useState(false);
+   // if isOpen is true, the navigation menu is open
    const links = [
       {
          href: "#features",
@@ -19,23 +23,37 @@ export const Navbar = () => {
          content: "Resources",
       }
    ];
+   const onMenuClick = () => {
+      setIsOpen((prevState) => !prevState);
+   };
    return (
-      <nav className="Navbar">
-         <section className="Navbar__icon">
-            <div className="line"></div>
-            <div className="line"></div>
-            <div className="line"></div>
+      <nav className={styles.Navbar}>
+         <section
+            className={`
+               ${styles.NavbarIcon}
+               ${isOpen && styles.icon__IsOpen}
+            `}
+            onClick={onMenuClick}
+         >
+            <div className={styles.iconLine}></div>
+            <div className={styles.iconLine}></div>
+            <div className={styles.iconLine}></div>
          </section>
-         <section className="Navbar__menu">
-            <section className="Navbar__links">
+         <section
+            className={`
+               ${styles.NavbarMenu}
+               ${isOpen ? styles.NavbarMenu__IsOpen : styles.NavbarMenu__IsClosed}
+            `}
+         >
+            <section className={styles.NavbarLinks}>
                {
                   links.map(
                      ({ href, content }) => <Link type="navlink" key={href} href={href} content={content} />
                   )
                }
             </section>
-            <hr className="Navbar__break-line" />
-            <section className="Navbar__buttons">
+            <hr className={styles.NavbarBreakLine} />
+            <section className={styles.NavbarButtons}>
                <Button type="ghost" content="Log in" />
                <Button type="rounded--small" content="Sign up" />
             </section>
