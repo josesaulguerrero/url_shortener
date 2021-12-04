a39f9cc636be78bdeb57fbe760cbe292df0ff6fc
Jose Saul Serrano <Jose Saul Serrano>
2021-12-04 08:44:48 -0500

    Created a Button component that lets the user copy the shortened link

--- src/components/LinkCard.js
+++ src/components/LinkCard.js
@@ -1,14 +1,14 @@
 //libraries and hooks
 //assets
 //components
-import { Button } from "./Button";
+import { CopyToClipboard } from "./copyToClipboard";
 
 export const LinkCard = ({ originalLink, ShortenedLink }) => {
    return (
       <article className="LinkCard">
          <p className="LinkCard__original-link">{originalLink}</p>
          <p className="LinkCard__shortened-link">{ShortenedLink}</p>
-         <Button type="squared--small" content="copy" />
+         <CopyToClipboard shortenedLink={ShortenedLink} />
       </article>
    );
 };
