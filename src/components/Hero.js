fea7885475df209d4618ddac2f4ac9b377551d4c
Jose Saul Serrano <Jose Saul Serrano>
2021-12-04 11:57:29 -0500

    The Hero was styled (with responsive styles)

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -3,14 +3,19 @@
 import { Button } from './Button';
 //assets
 import image from '../assets/images/illustration-working.svg';
+import styles from "../assets/styles/Hero.module.css";
 
 export const Hero = () => {
    return (
-      <main className="Hero">
-         <img src={image} alt="person working on a computer" width="80%" className="Hero__img" />
-         <h1 className="Hero__title">More than just shorter links</h1>
-         <p className="Hero__description">Build your brand's recognition and get detailed insights on how your links are performing.</p>
-         <Button type="rounded--large" content="Get started" />
+      <main className={styles.Hero}>
+         <section className={styles.HeroImg}>
+            <img src={image} alt="person working on a computer" width="80%" />
+         </section>
+         <section className={styles.HeroContent}>
+            <h1 className={styles.HeroTitle}>More than just shorter links</h1>
+            <p className={styles.HeroDescription}>Build your brand's recognition and get detailed insights on how your links are performing.</p>
+            <Button type="rounded--large" content="Get started" />
+         </section>
       </main>
    );
 };
