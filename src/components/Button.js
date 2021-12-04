52e236543c9e19067528e98b4e682ad5e0901b34
Jose Saul Serrano <Jose Saul Serrano>
2021-12-04 10:27:16 -0500

    the Button component was styled

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,12 +1,15 @@
 //libraries and hooks
 //components
 //assets
+import styles from "../assets/styles/Button.module.css";
 
 export const Button = ({
    type = "primary",
    content = "write something"
 }) => {
    return (
-      <button className={type}>{content}</button>
+      <button className={styles.Button} type={type} >
+         {content}
+      </button>
    );
 };
